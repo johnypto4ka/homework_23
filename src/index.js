23fabf70a326c265bb49c64854f2144a885d6592
Stas <johnyp9to4ka@mail.ru>
2022-03-24 16:02:50 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './scss/app.scss';
+
+
+const formElement = document.querySelector('#form')
+
+formElement.addEventListener('submit', (event) => {
+    console.log('submit')
+    if (!formElement.checkValidity()) {
+      event.preventDefault()
+      formElement.classList.add('enable-validation')
+    }
+  })
